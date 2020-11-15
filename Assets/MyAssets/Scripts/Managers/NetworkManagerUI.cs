e8196d0c61f74a0259eed7e240517dbd523466cc
Yongkun Li <indusdesign@163.com>
2020-11-14 23:30:32 -0500

    light edited

---
+++ Assets/MyAssets/Scripts/Managers/NetworkManagerUI.cs
@@ -0,0 +1,44 @@
+﻿#pragma warning disable 0649
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(NetworkManager))]
+public class NetworkManagerUI : MonoBehaviour
+{
+    NetworkManager manager;
+    [SerializeField]
+    Text InfoText;
+
+    InputManager inputManager;
+
+    void Awake()
+    {
+        manager = GetComponent<NetworkManager>();
+    }
+    
+    void Start() {
+        inputManager = InputManager.Instance;
+    }
+
+    public void LocalHost(){
+        manager.StartHost();
+        Cursor.lockState = CursorLockMode.Locked;
+        InfoText.text = "Local Host Mode";
+    }
+
+    public void ConnectSercer(){
+        manager.StartClient();
+        Cursor.lockState = CursorLockMode.Locked;
+        InfoText.text = "Connected to server";
+    }
+
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    void Update() {
+    }
+}
