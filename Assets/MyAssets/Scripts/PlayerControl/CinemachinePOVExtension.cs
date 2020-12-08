27ce73fe157ba3cc0fa808196c4e115e693220ff
Yongkun Li <indusdesign@163.com>
2020-12-07 21:21:08 -0500

    part of details refined

--- Assets/MyAssets/Scripts/PlayerControl/CinemachinePOVExtension.cs
+++ Assets/MyAssets/Scripts/PlayerControl/CinemachinePOVExtension.cs
@@ -4,9 +4,9 @@ using Cinemachine;
 public class CinemachinePOVExtension : CinemachineExtension
 {
     [SerializeField]
-    private float horizontalSpeed = 10f;
+    private float horizontalSpeed = 20f;
     [SerializeField]
-    private float verticalSpeed = 10f;
+    private float verticalSpeed = 20f;
     [SerializeField]
     private float clampAngle = 80f;
 
